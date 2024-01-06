a0447d5cffb2a1e42c1d1ec9392b2a8c50f55074
yulangz <1301481108@qq.com>
2024-01-07 00:22:59 +0800

    remove server cache logic

--- server/app.py
+++ server/app.py
@@ -18,9 +18,7 @@ record_cache_time = config['recordCacheTime']
 
 # 数据结构和锁
 client_records = {}
-client_cache = {}
 lock = threading.Lock()
-latest_update = datetime.now()
 
 @app.route('/route/sync', methods=['POST'])
 def sync():
@@ -31,17 +29,7 @@ def sync():
 
     with lock:
         # 更新映射和时间
-        if client_domain in client_records and client_records[client_domain]['ipv6'] == client_ipv6:
-            client_records[client_domain]['timestamp'] = datetime.now()
-        else:
-            client_records[client_domain] = {'ipv6': client_ipv6, 'timestamp': datetime.now()}
-            latest_update = datetime.now()
-
-        # 检查缓存
-        if client_domain in client_cache and not force:
-            last_time = client_cache[client_domain]
-            if latest_update < last_time and datetime.now() - last_time < timedelta(minutes=client_cache_time):
-                return jsonify({})
+        client_records[client_domain] = {'ipv6': client_ipv6, 'timestamp': datetime.now()}
 
         # 清除过期记录
         current_time = datetime.now()
@@ -49,7 +37,6 @@ def sync():
             if current_time - client_records[domain]['timestamp'] > timedelta(minutes=record_cache_time):
                 del client_records[domain]
 
-        client_cache[client_domain] = current_time
         return jsonify({domain: record['ipv6'] for domain, record in client_records.items()})
 
 if __name__ == '__main__':
